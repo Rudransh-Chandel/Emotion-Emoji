b34b6ea81f4f5faf69249eefb31c3db806c2b8de
Rudransh-Chandel <106671046+Rudransh-Chandel@users.noreply.github.com>
2023-11-06 19:27:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -36,3 +36,49 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speak1 + speak2);
     synth.speak(utterThis);
 }
+
+function check()
+{
+    img = document.getElementById("imgcaptured");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if (error)
+{
+    console.error(error);
+} else {
+    console.log(results);
+    document.getElementById("emotionName").innerHTML = results[0].label;
+    document.getElementById("emotionNaeme2").innerHTML = results[1].label;
+    prediction1 = results[0].label;
+    prediction2 = results[1].label;
+    speak();
+    if(results[0].label == "Happy")
+    {
+        document.getElementById("emojiEmotion").innerHTML = "&#128522;"
+    }
+    if(results[0].label == "Sad")
+    {
+        document.getElementById("emojiEmotion").innerHTML = "&#128532;"
+    }
+    if(results[0].label == "Angry")
+    {
+        document.getElementById("emojiEmotion").innerHTML = "&#128548;"
+    }
+
+    if(results[1].label == "Happy")
+    {
+        document.getElementById("emojiEmotion2").innerHTML = "&#128522;"
+    }
+    if(results[1].label == "Sad")
+    {
+        document.getElementById("emojiEmotion2").innerHTML = "&#128532;"
+    }
+    if(results[1].label == "Angry")
+    {
+        document.getElementById("emojiEmotion2").innerHTML = "&#128548;"
+    }
+
+}
+}
